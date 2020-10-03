0957413bb1d001c8c49ff269adbe7b7e407b9ae3
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-03 19:43:40 +0200

    Skriver brukerhistorier for alle oppgavene

--- test/kalkulator.test.js
+++ test/kalkulator.test.js
@@ -24,3 +24,51 @@ test('addisjon av 1 + 3 gir 4', t => {
 test('addisjon av 1 + 2 gir 3', t => {
   t.is(addisjon(1, 2), 3, 'addisjon 1 + 2 er ok')
 })
+
+/**
+ * Som bruker ønsker jeg at subtraksjon av 3 - 1 gir svaret 2
+ * for at jeg skal kunne vite at kalkulatoren min fungerer
+ */
+
+// ---- skriv testen for denne brukerhistorien her
+
+/**
+ * Som bruker ønsker jeg at subtraksjon av 5 - 1 gir svaret 4
+ * for at jeg skal kunne vite at kalkulatoren min fungerer
+ */
+
+// ---- skriv testen for denne brukerhistorien her
+
+/**
+ * Som bruker ønsker jeg at multiplikasjon av 2 * 2 gir svaret 4
+ * for at jeg skal kunne vite at kalkulatoren min fungerer
+ */
+
+// ---- skriv testen for denne brukerhistorien her
+
+/**
+ * Som bruker ønsker jeg at multiplikasjon av 2 * 4 gir svaret 8
+ * for at jeg skal kunne vite at kalkulatoren min fungerer
+ */
+
+// ---- skriv testen for denne brukerhistorien her
+
+/**
+ * Som bruker ønsker jeg at divisjon av 10/5 gir svaret 2
+ * for at jeg skal kunne vite at kalkulatoren min fungerer
+ */
+
+// ---- skriv testen for denne brukerhistorien her
+
+/**
+ * Som bruker ønsker jeg at divisjon av 10/2 gir svaret 5
+ * for at jeg skal kunne vite at kalkulatoren min fungerer
+ */
+
+// ---- skriv testen for denne brukerhistorien her
+
+/**
+ * BONUSOPPGAVE
+ * Kan du skrive en test som tester addisjon med alle tall fra 1 til 100?
+ * Hint: Tester er bare funksjoner og funksjoner kan kalle andre funksjoner 
+ */

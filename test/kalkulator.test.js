67465ea783f1f0901fcf7aa786a4d4a279767f05
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-03 19:13:42 +0200

    Skriver om koden så den ligner mer på det de kjenner i dag

--- test/kalkulator.test.js
+++ test/kalkulator.test.js
@@ -3,17 +3,18 @@
  * Man oppgir navnet på modulen dersom man har installert den slik vi gjør med ava
  * Man oppgir banen til modulen dersom der er en lokal fil
  * ".." betyr opp et nivå i katalogstrukturen, dersom du skal hente en fil i samme katalog du står så bruker du "./"
+ * { navn } brukes for å sette flere variabler samtidig
  */
 
 const test = require('ava')
-const kalkulator = require('../lib/kalkulator')()
+const { addisjon } = require('../lib/kalkulator')
 
 /**
  * Som bruker ønsker jeg at addisjon av 1 + 3 gir svaret 4
  * for at jeg skal kunne vite at kalkulatoren min fungerer
  */
 test('addisjon av 1 + 3 gir 4', t => {
-  t.is(kalkulator.addisjon(1, 3), 4, 'addisjon 1 + 3 er ok')
+  t.is(addisjon(1, 3), 4, 'addisjon 1 + 3 er ok')
 })
 
 /**
@@ -21,5 +22,5 @@ test('addisjon av 1 + 3 gir 4', t => {
  * for at jeg skal kunne vite at kalkulatoren min fungerer
  */
 test('addisjon av 1 + 2 gir 3', t => {
-  t.is(kalkulator.addisjon(1, 2), 3, 'addisjon 1 + 2 er ok')
+  t.is(addisjon(1, 2), 3, 'addisjon 1 + 2 er ok')
 })
